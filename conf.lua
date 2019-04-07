b403378ea982fdca26a231303326c6a3c2da829a
ksergeant <kevin.sergeant.pro@gmail.com>
2019-04-07 20:58:33 +0200

    fix

--- conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-  
-    t.window.width = 1440
-    t.window.height = 800
-    t.window.x = 0
-    t.window.y = 0
-    t.window.display = 2
-    t.window.title = "Map Aventure"
-  end
